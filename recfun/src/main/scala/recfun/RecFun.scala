b57dd5e4c8658bcd2fc2737d40b3c3503608a4a7
supi <supi@supis-MacBook-Pro.local>
2023-12-30 16:17:49 -0400

    examples and tasks

---
+++ recfun/src/main/scala/recfun/RecFun.scala
@@ -0,0 +1,44 @@
+package recfun
+
+import scala.annotation.tailrec
+
+object RecFun extends RecFunInterface:
+
+  def main(args: Array[String]): Unit =
+    println("Pascal's Triangle")
+    for row <- 0 to 10 do
+      for col <- 0 to row do
+        print(s"${pascal(col, row)} ")
+      println()
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
+  }
+
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = {
+    @tailrec
+    def balanceHelper(chars: List[Char], openCount: Int): Boolean = {
+      if (chars.isEmpty) openCount == 0
+      else if (chars.head == '(') balanceHelper(chars.tail, openCount + 1)
+      else if (chars.head == ')') openCount > 0 && balanceHelper(chars.tail, openCount - 1)
+      else balanceHelper(chars.tail, openCount)
+    }
+
+    balanceHelper(chars, 0)
+  }
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0) 1
+    else if (money < 0 || coins.isEmpty) 0
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+  }
